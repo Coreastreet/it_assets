92c6893b5de925b06b5dc8a2d6ed18adf797f3b0
justin <hi_justin@hotmail.com>
2024-01-25 12:26:38 +1100

    final draft

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,1 +1,15 @@
 console.log("working from script.js")
+
+$(".vertical-nav li.nav-item").on("click", function() {
+    $(this).siblings().each(function() {
+        $(this).children(":first").removeClass("active");
+    })
+    $(this).children(":first").addClass("active");
+})
+
+$(".second-sticky li.nav-item").on("click", function() {
+    $(this).siblings().each(function() {
+        $(this).children(":first").removeClass("active");
+    })
+    $(this).children(":first").addClass("active");
+})
